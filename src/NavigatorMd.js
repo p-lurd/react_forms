bbfed31511b7621f897a3ca40ee39b6cd60e100e
Jedidiah <obayorit@gmail.com>
2024-09-03 09:18:40 +0100

    navigation

--- src/NavigatorMd.js
+++ src/NavigatorMd.js
@@ -19,14 +19,13 @@ const Navigator = () => {
     const currentIndex = getCurrentRouteIndex();
     return currentIndex > 0 ? routes[currentIndex - 1] : null;
   };
-  // console.log("name", name);
+  
   const next = getNextPath();
   const prev = getPreviousPath();
-  // console.log("locations", next, prev);
-    // console.log("next", next)
+  console.log(location)
     return ( 
         <div className="md:relative fixed flex justify-between bg-white bottom-0 left-0 w-full pt-5 mt-5 ">
-            <Link className="block m-3 p-2 text-gray-400" to={prev}>Go Back</Link>
+            <Link className={`m-3 p-2 text-gray-400 ${location.pathname === '/personal_info' ? 'hidden' : 'block'}`} to={prev}>Go Back</Link>
             <Link className="block m-3 p-2 text-white bg-blue-900 rounded-md" to={next}>Next Step</Link>
 
         </div>
